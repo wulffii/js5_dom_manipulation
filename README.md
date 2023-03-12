841a2194b2b57dba1d8d33380bafc8ba169b6010
mahaUser2 <75357482+mahaUser2@users.noreply.github.com>
2023-03-12 17:20:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# js5_dom_manipulation
+ 
---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Opgave 1: sørg for at man ved klik på ☼/◯ (findes i nav > ul > .toggleColormode) skifter imellem light og darkmode. Se at <body> har en class der matcher det i style.css
+
+// Opgave 2: sørg for at gemme det i localStorage så brugerens indstillinger gemmes
+
+// Opgave 3: Hver person har en <h4> inde i <main>. Den er dog meget dum og der står bare "Look at me". Du skal istedet opdatere <h4> med det der står i billedets alt-attribute. For den første person skal der altså stå: "Jim the Chef". Anvend for guds skyld loops/løkker til det!
+
+// Opgave 4: skab og indsæt i bunden (under nederste person) et link der fører til toppen af siden (så href="#" i linket).
